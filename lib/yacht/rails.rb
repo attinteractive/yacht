d38f82545b17c6372ec6e1e0b67d771cd9870674
Mani Tadayon <bowsersenior@gmail.com>
2011-06-29 16:22:41 -0700

    Add :rails_env key when Rails is defined

--- lib/yacht/rails.rb
+++ lib/yacht/rails.rb
@@ -12,5 +12,12 @@ class Yacht::Loader
     def full_file_path_for_config(config_type)
       dir.join("#{config_type}.yml")
     end
+
+    # Add current Rails environment to defined keys
+    def all_with_rails_env
+      all_without_rails_env.merge(:rails_env => Rails.env)
+    end
+    alias_method_chain :all, :rails_env
+
   end
 end
--- spec/yacht/rails_spec.rb
+++ spec/yacht/rails_spec.rb
@@ -4,6 +4,7 @@ describe "Rails support" do
   subject { Yacht::Loader }
 
   before do
+    Object.stub(:alias_method_chain).as_null_object
     require "yacht/rails"
     @yacht_dir = "/path/to/rails/config/yacht"
   end
@@ -34,6 +35,20 @@ describe "Rails support" do
       fake_dir.should_receive(:join).with("some.yml")
 
       subject.full_file_path_for_config("some")
+    end
+  end
+
+  describe :all_with_rails_env do
+    it "adds the current Rails environment to super" do
+      subject.stub(:all_without_rails_env).and_return(:foo => :bar)
+
+      Rails.stub(:env).and_return(:awesome)
+      subject.all_with_rails_env.should == {:foo => :bar, :rails_env => :awesome}
+    end
+
+    it "aliases all to all_without_rails_env" do
+      Object.should_receive(:alias_method_chain).with(:all, :rails_env)
+      load "yacht/rails.rb"
     end
   end
 end
