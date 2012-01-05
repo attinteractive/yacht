50925437a5b60fb0de1522e4ab7a741d2c9105aa
Mani Tadayon <bowsersenior@gmail.com>
2012-01-05 15:54:04 -0800

    Remove Rails support features that automatically set directory and environment
    This feature was complicated to implement and test. It can be
    replicated with a simple 2-line initializer within a Rails app. In
    hindsight, this feature shouldn't have been added in the first place.

--- lib/monkeypatches/rails_helper.rb
+++ lib/monkeypatches/rails_helper.rb
@@ -1,8 +1,6 @@
 module Yacht::RailsHelper
   # Create a string with a javascript version of Yacht values intended for inclusion in an HTML page
   #
-  # @note environment will be set to Rails.env by default
-  #
   # @example Set custom environment
   #   yacht_js_snippet(:env => 'local_development')
   #   # => "<script type=\"text/javascript\">;var Yacht = {\"foo\":\"bar\"};</script>"
--- lib/yacht.rb
+++ lib/yacht.rb
@@ -4,14 +4,6 @@ require "yacht/base"
 require "yacht/loader"
 
 if Object.const_defined?(:Rails)
-  require "yacht/rails"
-  class Yacht::Loader
-    include Yacht::Rails
-  end
-
-  Yacht::Loader.dir          = Yacht::Loader.rails_default_yacht_dir
-  Yacht::Loader.environment  = Yacht::Loader.rails_env
-
   require "monkeypatches/rails_helper"
 end
 
